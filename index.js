d8a85d40220f5f657f25087b5a82909e6fba0dbb
Edson Alcala <edson_alcala@msn.com>
2017-12-24 11:01:29 +0000

    i dont need api, just trigger all on launch

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const config = require('./database/config');
 const connectDatabase = require('./database/connection');
 const app = new Koa();
 const cors = require('koa-cors');
+const queries = require('./database/queries');
+const Scrapper = require('./utils/scrapper');
 
-//todo error handler
 app.use(cors());
 app.use(indexRoutes.routes());
 
 const server = app.listen(PORT, async () => {
   try {
     const info = await connectDatabase(config.db);
-    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);        
+    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);    
+    
+    let scrapper = new Scrapper();             
+    const itemsToInsert = await scrapper.getAllData();
+    const cleanDb = await queries.cleanDatabase();
+    const savedItems = await queries.saveToDatabase(itemsToInsert);
+
+    console.log(`All inserted properly`);   
+
   } catch (error) {
     console.error('Unable to connect to database');
   }
